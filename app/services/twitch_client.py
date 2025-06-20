74328a078b2b6509e79d6c04f2ae0a6afb0735c4
Ivan <ivanarteta97@gmail.com>
2025-06-20 11:38:01 +0200

    refactor para reintentar peticion si token no valido

--- app/services/twitch_client.py
+++ app/services/twitch_client.py
@@ -34,16 +34,29 @@ async def get_token():
     ACCESS_TOKEN = response.json().get("access_token")
     return ACCESS_TOKEN   
 
-async def get_user_info(id):
-    token = await get_token()
+
+async def twitch_request(method: str, endpoint: str, params=None, retry=True):
+    global ACCESS_TOKEN
+
+    if ACCESS_TOKEN is None:
+        await get_token()
 
     headers = {
-        "Authorization": f"Bearer {token}",
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
         "Client-Id": CLIENT_ID
-    }
+    }     
 
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"{BASE_URL}/users", params={"id": id}, headers=headers)
+        response = await client.get(f"{BASE_URL}{endpoint}", params=params, headers=headers)
+
+    if response.status_code == 401 and retry:
+        ACCESS_TOKEN = None
+        return await twitch_request(method, endpoint, params=params, retry=False)
+
+    return response
+
+async def get_user_info(id):
+    response = await twitch_request("GET", "/users", params={"id": id})
 
     if response.status_code == 401:
         raise_http_error(401, "Unauthorized. Twitch access token is invalid or has expired.")
@@ -55,15 +68,7 @@ async def get_user_info(id):
     return response.json().get("data", [])[0]
 
 async def get_live_streams():
-    token = await get_token()
-
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Client-Id": CLIENT_ID
-    }
-
-    async with httpx.AsyncClient() as client:
-        response = await client.get(f"{BASE_URL}/streams", headers=headers)
+    response = await twitch_request("GET", "/streams")
 
     if response.status_code == 401:
         raise_http_error(401, "Unauthorized. Twitch access token is invalid or has expired.")
